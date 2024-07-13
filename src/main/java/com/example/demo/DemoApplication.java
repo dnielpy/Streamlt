ca9fcc74f9c71a0f237bd3641cbeb5161cf2aa79
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-07-12 23:12:49 -0400

    algunos cambios

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
+		// Abre el navegador automáticamente después de iniciar el servidor
+		try {
+			Runtime.getRuntime().exec("cmd /c start http://localhost:8080");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
